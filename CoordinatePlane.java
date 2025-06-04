602b57548ca51d15e4ca08137079dcf94e129d57
Demetri Karras <108017719+demetri894@users.noreply.github.com>
2025-06-04 10:35:15 -0400

    cleaned up code and added comments

--- CoordinatePlane.java
+++ CoordinatePlane.java
@@ -1,16 +1,23 @@
 import java.util.*;
 
+/*
+A 2D coordinate plane with user-specified bounds and number of points.
+ */
 public class CoordinatePlane {
 
     final int numPoints;
-    final int bounds;
+    final int bounds; // the bounds of the plane in all directions (X, -X, Y, -Y)
     private final List<Point> pointsSortedNonDecrByX = new ArrayList<>();
     private final List<Point> pointsSortedNonDecrByY = new ArrayList<>();
 
-    public CoordinatePlane(int numPoints, int bounds) throws Exception {
+    public CoordinatePlane(int numPoints, int bounds) throws IllegalArgumentException {
 
+        // throws exception if plane cannot be built
         if (numPoints > Math.pow((bounds * 2) + 1, 2)) {
-            throw new Exception(String.format("%d points cannot fit in a plane with a bounds of %d.", numPoints, bounds));
+            throw new IllegalArgumentException(String.format("%d points cannot fit in a plane with a bounds of %d.", numPoints, bounds));
+        }
+        else if (numPoints < 3) {
+            throw new IllegalArgumentException("Number of points must be at least 3.");
         }
 
         this.numPoints = numPoints;
@@ -18,91 +25,136 @@ public class CoordinatePlane {
         buildPlane();
     }
 
+    /*
+    Builds a coordinate plane by randomly generating points within a certain bounds.
+
+    Input: none
+    Output: none - the lists of points are updated
+     */
     private void buildPlane() {
 
-        Point point;
+        Random rand = new Random();
 
-        for (int i = 0; i < numPoints; i++) {
-            point = getUniqueRandomPoint();
-            pointsSortedNonDecrByX.add(point);
-            pointsSortedNonDecrByY.add(point);
+        Set<Point> pointSet = new HashSet<>(); // a set is used to store the generated points to prevent duplicates
+
+        // adds randomly generated points to the point set
+        while (pointSet.size() < numPoints) {
+
+            int x = rand.nextInt(bounds * -1, bounds + 1);
+            int y = rand.nextInt(bounds * -1, bounds + 1);
+
+            pointSet.add(new Point(x, y));
         }
 
+        // transfers points from the set into lists
+        pointsSortedNonDecrByX.addAll(pointSet);
+        pointsSortedNonDecrByY.addAll(pointSet);
+
+        // sorts the lists in ascending order based on X or Y coordinates respectively
         pointsSortedNonDecrByX.sort((p1, p2) -> Integer.compare(p1.x, p2.x));
         pointsSortedNonDecrByY.sort((p1, p2) -> Integer.compare(p1.y, p2.y));
     }
 
-    private Point getUniqueRandomPoint() {
-        Random rand = new Random();
-        int x = rand.nextInt(bounds * -1, bounds + 1);
-        int y = rand.nextInt(bounds * -1, bounds + 1);
+    /*
+    Contains the main logic of the Divide and Conquer algorithm to solve the 2D Closest Pair problem.
 
-        Point point = new Point(x, y);
-        if (pointsSortedNonDecrByX.contains(point)) {
-            return getUniqueRandomPoint();
-        }
-        return point;
-    }
+    The plane is recursively split in half by X. The closest pair within each half is computed using brute force when
+    numPoints <= 3. A vertical strip around the dividing line is examined to see if a closer pair exists between points
+    on each side.
 
-    double findClosestPair(List<Point> pointsSortedNonDecrByX, List<Point> pointsSortedNonDecrByY) {
+    Input: 2 lists of points, sorted by X and Y respectively
+    Output: the closest pair and their distance among the provided points
+     */
+    Solution findClosestPair(List<Point> pointsSortedNonDecrByX, List<Point> pointsSortedNonDecrByY) throws Exception {
 
+        Solution solution;
         Point[] closestPair = new Point[2];
-        List<Point> p, pLeft, pRight;
-        List<Point> q, qLeft, qRight;
-        List<Point> s;
-        double minDistance, minDistanceSquared = 0, minDistanceLeft, minDistanceRight;
-        int midpointX;
+        double minDistance, minDistanceSquared;
 
-        p = pointsSortedNonDecrByX;
+        List<Point> p = pointsSortedNonDecrByX;
 
         if (p.size() <= 3) {
             return findClosestPairByBruteForce(p);
         }
         else {
-            q = pointsSortedNonDecrByY;
-            pLeft = p.subList(0, p.size() / 2);
-            qLeft = q.stream().filter(point -> pLeft.contains(point)).toList();
-            pRight = p.subList(p.size() / 2, p.size());
-            qRight = q.stream().filter(point -> pRight.contains(point)).toList();
 
-//            System.out.println(Arrays.toString(p.toArray()));
-//            System.out.println(Arrays.toString(q.toArray()));
-//            System.out.println();
-//
-//            System.out.println(Arrays.toString(pLeft.toArray()));
-//            System.out.println(Arrays.toString(qLeft.toArray()));
-//            System.out.println();
-//
-//            System.out.println(Arrays.toString(pRight.toArray()));
-//            System.out.println(Arrays.toString(qRight.toArray()));
-//            System.out.println();
+            List<Point> q = pointsSortedNonDecrByY;
 
-            minDistanceLeft = findClosestPair(pLeft, qLeft);
-            minDistanceRight = findClosestPair(pRight, qRight);
-            minDistance = Math.min(minDistanceLeft, minDistanceRight);
+            List<Point> pLeft = p.subList(0, p.size() / 2); // left half of p sorted by X
+            Set<Point> pLeftSet = new HashSet<>(pLeft); // set is created and used to avoid nested linear operations
 
-            midpointX = p.get(p.size() / 2 - 1).x;
+            List<Point> qLeft = q.stream() // left half of p sorted by Y
+                    .filter(point -> pLeftSet.contains(point))
+                    .toList();
 
-            s = q.stream().filter(point -> Math.abs(point.x - midpointX) < minDistance).toList();
+            List<Point> pRight = p.subList(p.size() / 2, p.size()); // right half of p sorted by X
+            Set<Point> pRightSet = new HashSet<>(pRight); // set is created and used to avoid nested linear operations
+
+            List<Point> qRight = q.stream() // right half of p sorted by Y
+                    .filter(point -> pRightSet.contains(point))
+                    .toList();
+
+            // finds the closest pair in the left and right regions containing (numPoints / 2) points
+            Solution solutionLeft = findClosestPair(pLeft, qLeft);
+            Solution solutionRight = findClosestPair(pRight, qRight);
+
+            // finds the smallest distance among the two halves and builds the corresponding solution
+            if (solutionLeft.getDistance() <= solutionRight.getDistance()) {
+                minDistance = solutionLeft.getDistance();
+                closestPair[0] = solutionLeft.getPair()[0];
+                closestPair[1] = solutionLeft.getPair()[1];
+            }
+            else {
+                minDistance = solutionRight.getDistance();
+                closestPair[0] = solutionRight.getPair()[0];
+                closestPair[1] = solutionRight.getPair()[1];
+            }
+
+            int midX = p.get(p.size() / 2 - 1).x; // represents the vertical line dividing the examined space in half
+
+            // stores all points within the current minimum distance of the dividing line
+            List<Point> s = q.stream()
+                    .filter(point -> Math.abs(point.x - midX) < minDistance)
+                    .toList();
 
             minDistanceSquared = minDistance * minDistance;
+            // iterates through relevant points within the minimum distance to check for closer pairs
             for (int i = 0; i < s.size() - 2; i++) {
-                int k = i +1;
-                while (k <= s.size() - 1 && (Math.pow(s.get(k).y - s.get(i).y, 2) < minDistanceSquared)) {
-                    minDistanceSquared = Math.min(Math.pow(s.get(k).x - s.get(i).x, 2) + Math.pow(s.get(k).y - s.get(i).y, 2), minDistanceSquared);
+                int k = i + 1;
+                while (k <= s.size() - 1 && (Math.pow(s.get(k).y - s.get(i).y, 2) < minDistanceSquared)) { // ensures that only points within a relevant Y distance from each other are checked
+                    // if the newly calculated distance is less than the previous minimum, this pair becomes the new solution
+                    if (Math.pow(s.get(k).x - s.get(i).x, 2) + Math.pow(s.get(k).y - s.get(i).y, 2) < minDistanceSquared) {
+                        minDistanceSquared = Math.pow(s.get(k).x - s.get(i).x, 2) + Math.pow(s.get(k).y - s.get(i).y, 2);
+                        closestPair[0] = s.get(i);
+                        closestPair[1] = s.get(k);
+                    }
                     k++;
                 }
             }
         }
 
-        return Math.sqrt(minDistanceSquared);
+        solution = new Solution(closestPair, Math.sqrt(minDistanceSquared));
+
+        return solution;
     }
 
-    private double findClosestPairByBruteForce(List<Point> pointsSortedNonDecrByX) {
+    /*
+    Finds the closest pair of points for lists of 2 or 3 points by Brute Force (testing every combination).
+
+    Input: list of 2 or 3 points for which to find the closest pair
+    Output: the closest pair and its distance
+     */
+    private Solution findClosestPairByBruteForce(List<Point> pointsSortedNonDecrByX) throws Exception {
 
         List<Point> p = pointsSortedNonDecrByX;
 
-        if (p.size() == 3) {
+        Point[] closestPair = new Point[2];
+        double minDistance;
+        Solution solution;
+
+        if (p.size() == 3) { // finds the closest pair among 3 points
+
+            // gets the distances between all combinations of points and finds the minimum
             double d1 = getDistance(p.get(0), p.get(1));
             double d2 = getDistance(p.get(1), p.get(2));
             double d3 = getDistance(p.get(0), p.get(2));
@@ -110,16 +162,46 @@ public class CoordinatePlane {
             List<Double> distances = new ArrayList<>();
             distances.add(d1); distances.add(d2); distances.add(d3);
 
-            return Collections.min(distances);
+            minDistance = Collections.min(distances);
+
+            // builds and returns a Solution based on the closest pair
+            if (minDistance == d1) {
+                closestPair[0] = p.get(0);
+                closestPair[1] = p.get(1);
+            }
+            else if (minDistance == d2) {
+                closestPair[0] = p.get(1);
+                closestPair[1] = p.get(2);
+            }
+            else {
+                closestPair[0] = p.get(0);
+                closestPair[1] = p.get(2);
+            }
+
+            solution = new Solution(closestPair, minDistance);
+
+            return solution;
         }
-        else if (p.size() == 2) {
-            return getDistance(p.get(0), p.get(1));
+        else if (p.size() == 2) { // the closest pair among 2 points is those 2 points
+
+            closestPair[0] = p.get(0);
+            closestPair[1] = p.get(1);
+
+            solution = new Solution(closestPair, getDistance(p.get(0), p.get(1)));
+
+            return solution;
         }
         else {
-            return 0;
+            throw new Exception("Method <findClosestPairByBruteForce> can ONLY solve lists with 2 or 3 points.");
         }
     }
 
+    /*
+    Calculates the distance between two points using the Euclidean distance formula.
+
+    Input: 2 points for which to calculate their distance
+    Output: the distance between the specified points
+     */
     private double getDistance(Point p1, Point p2) {
 
         int d1 = p2.x - p1.x;
@@ -147,7 +229,10 @@ public class CoordinatePlane {
         return result;
     }
 
-    public class Point {
+    /*
+    Represents a point on the coordinate plane.
+     */
+    public static class Point {
         int x, y;
 
         public Point(int x, int y) {
@@ -155,6 +240,13 @@ public class CoordinatePlane {
             this.y = y;
         }
 
+        /*
+        Compares the X and Y values of two Point objects for equality. Original method was overridden to allow for
+        correct sorting in a collection of points.
+
+        Input: Object (Point) to be compared
+        Output: boolean representing the equality of points
+         */
         @Override
         public boolean equals(Object obj) {
             if (obj instanceof Point) {
@@ -163,8 +255,48 @@ public class CoordinatePlane {
             return false;
         }
 
+        /*
+        Returns the hash code of a point object based on X and Y coordinates. Original method was overridden to allow
+        for correct duplicate detection in a set.
+
+        Input: none
+        Output: the hash code representing the point
+         */
+        @Override
+        public int hashCode() {
+            return Objects.hash(x, y);
+        }
+
+        /*
+        Returns a string representation of the point formatted as follows: (x, y)
+
+        Input: none
+        Output: the string representation of the point
+         */
         public String toString() {
             return "(" + x + ", " + y + ")";
+        }
+    }
+
+    /*
+    Represents the solution to the Closest Pair problem. Contains the two closest points and their distance apart.
+     */
+    public static class Solution {
+
+        private final Point[] pair;
+        private final double distance;
+
+        public Solution(Point[] pair, double distance) {
+            this.pair = pair;
+            this.distance = distance;
+        }
+
+        Point[] getPair() {
+            return pair;
+        }
+
+        double getDistance() {
+            return distance;
         }
     }
 }
--- Solver2DClosestPair.java
+++ Solver2DClosestPair.java
@@ -1,3 +1,13 @@
+/*
+== Project 2 ==
+
+Author: Demetri Karras
+Class: CS203
+Due: June 12th, 2025
+ */
+
+import java.text.DecimalFormat;
+import java.util.List;
 import java.util.Scanner;
 
 public class Solver2DClosestPair {
@@ -5,28 +15,54 @@ public class Solver2DClosestPair {
 
         Scanner scan = new Scanner(System.in);
 
+        // explains the program and obtains input
         System.out.println("""
+                === FIND CLOSEST PAIR ===
                 You are going to create a coordinate plane by specifying the number of points and the bounds.
                 Points will be randomly generated within that bounds, and can only take integer values.
+                For relevant results, it is recommended that { number_of_points / (4(bounds * bounds)) < 0.5 } to
+                prevent collisions when generating random points for the board.
                 """);
 
         System.out.print("What is the bounds of the coordinate plane? ");
         int bounds = scan.nextInt();
-        System.out.print("How many random points should be generated? ");
+        System.out.print("How many random points should be generated (n > 2)? ");
         int numPoints = scan.nextInt();
         System.out.println();
 
+        long startTime = System.nanoTime();
+
         CoordinatePlane plane = null;
 
+        // coordinate plane is built, points are randomly generated within the bounds and are sorted
         try {
             plane = new CoordinatePlane(numPoints, bounds);
+        } catch (IllegalArgumentException iae) {
+            System.out.println(iae.getMessage());
+            System.exit(0);
         }
-        catch (Exception e) {
+
+        // lists of points are obtained from the plane
+        List<CoordinatePlane.Point> pointsSortedNonDecrByX = plane.getPointsSortedNonDecrByX();
+        List<CoordinatePlane.Point> pointsSortedNonDecrByY = plane.getPointsSortedNonDecrByY();
+
+        CoordinatePlane.Solution solution = null;
+
+        // the main algorithm is run, finding the closest pair within the plane
+        try {
+            solution = plane.findClosestPair(pointsSortedNonDecrByX, pointsSortedNonDecrByY);
+        } catch (Exception e) {
             System.out.println(e.getMessage());
             System.exit(0);
         }
 
-        System.out.println(plane);
-        System.out.println(plane.findClosestPair(plane.getPointsSortedNonDecrByX(), plane.getPointsSortedNonDecrByY()));
+        long endTime = System.nanoTime();
+
+        long elapsedMilliseconds = (endTime - startTime) / 1000000;
+
+        // the solution is printed to the user
+        DecimalFormat df = new DecimalFormat("#.###");
+        System.out.println("Closest Pair: " + solution.getPair()[0] + " and " + solution.getPair()[1] + "\nDistance: " + df.format(solution.getDistance()));
+        System.out.println("Runtime: " + elapsedMilliseconds + " ms");
     }
 }
